d0b31a6e2bc390182a0c7aa871b2279a4486ac90
Edwin Richbald A. Salinas <edos.004@gmail.com>
2026-02-14 17:43:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -44,15 +44,10 @@ async function initMap() {
       position: { lat: locData.lat, lng: locData.lng },
       icon: {
         url: "data:image/svg+xml;charset=UTF-8," + encodeURIComponent(`
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='url(#gradient)'>
-            <defs>
-              <linearGradient id='gradient' x1='0%' y1='0%' x2='0%' y2='100%'>
-                <stop offset='0%' style='stop-color:#f43f5e;stop-opacity:1' />
-                <stop offset='100%' style='stop-color:#e11d48;stop-opacity:1' />
-              </linearGradient>
-            </defs>
-            <path d='M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z'/>
-          </svg>`),
+  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#e11d48'>
+    <path d='M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z'/>
+  </svg>
+`),
         scaledSize: new google.maps.Size(40, 40),
         anchor: new google.maps.Point(20, 40)
       },
